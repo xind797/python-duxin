695794d3bd3afb221d4c12b9f0e4329c476e9fd8
xind797 <Xin.Du@metropolia.fi>
2024-08-22 15:36:13 +0300

    ok

--- metropolia_python.py
+++ metropolia_python.py
@@ -1,20 +1,22 @@
 import math
-'''
+#1
 print("Hello, World!")
 print("hello,duxin")
 print("hello,杜鑫")
 name=input("my name is:")
 print(f"hello,{name}")
+#2
 rds=float(input("input radius:"))
 area=math.pi*rds*rds
 print(f"area={area:.2f}")
+#3
 length=float(input("input length:"))
 width=float(input("input width:"))
 perimeter=2*length+2*width
 area=length*width
 print(f"perimeter={perimeter:.2f}")
 print(f"area={area:.2f}")
-
+#4
 num1=float(input("input num1:"))
 num2=float(input("input num2:"))
 num3=float(input("input num3:"))
@@ -22,7 +24,7 @@ sum=num1+num2+num3
 product=num1*num2*num3
 average=sum/3
 print(f"sum={sum:.1f}, product={product:.1f}, average={average:.1f}")
-
+#5
 talents=float(input("input talent:"))
 pounds=float(input("input pounds:"))
 lots=float(input("input lots:"))
@@ -30,7 +32,7 @@ mass1=lots*13.3+pounds*32*13.3+talents*20*32*13.3
 mass2=(mass1//1000)
 mass3=(mass1%1000)
 print(f"The weight in modern units:{mass2}kilograms and {mass3:2f} grams.")
-'''
+#6
 import random
 num1=[random.randint(0,9),random.randint(0,9),random.randint(0,9)]
 num2=[random.randint(1,6),random.randint(1,6),random.randint(1,6),random.randint(1,6)]
