0eed6a08bd1aba61f83b2d234b1475b12aa6d9f0
xind797 <Xin.Du@metropolia.fi>
2024-08-22 15:33:39 +0300

    ok

--- metropolia_python.py
+++ metropolia_python.py
@@ -14,4 +14,30 @@ perimeter=2*length+2*width
 area=length*width
 print(f"perimeter={perimeter:.2f}")
 print(f"area={area:.2f}")
+
+num1=float(input("input num1:"))
+num2=float(input("input num2:"))
+num3=float(input("input num3:"))
+sum=num1+num2+num3
+product=num1*num2*num3
+average=sum/3
+print(f"sum={sum:.1f}, product={product:.1f}, average={average:.1f}")
+
+talents=float(input("input talent:"))
+pounds=float(input("input pounds:"))
+lots=float(input("input lots:"))
+mass1=lots*13.3+pounds*32*13.3+talents*20*32*13.3
+mass2=(mass1//1000)
+mass3=(mass1%1000)
+print(f"The weight in modern units:{mass2}kilograms and {mass3:2f} grams.")
 '''
+import random
+num1=[random.randint(0,9),random.randint(0,9),random.randint(0,9)]
+num2=[random.randint(1,6),random.randint(1,6),random.randint(1,6),random.randint(1,6)]
+print(num1)
+print(num2)
+num3=[random.randint(0,9),random.randint(0,9),random.randint(0,9)]
+num4=[random.randint(1,6),random.randint(1,6),random.randint(1,6),random.randint(1,6)]
+print(num3)
+print(num4)
+
