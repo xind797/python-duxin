cd3131fc1cef41deb37265937bbd98dd4b813bf1
xind797 <Xin.Du@metropolia.fi>
2024-08-22 14:43:26 +0300

    ok

--- metropolia_python.py
+++ metropolia_python.py
@@ -1,2 +1,17 @@
+import math
+'''
 print("Hello, World!")
-print(123)
+print("hello,duxin")
+print("hello,杜鑫")
+name=input("my name is:")
+print(f"hello,{name}")
+rds=float(input("input radius:"))
+area=math.pi*rds*rds
+print(f"area={area:.2f}")
+length=float(input("input length:"))
+width=float(input("input width:"))
+perimeter=2*length+2*width
+area=length*width
+print(f"perimeter={perimeter:.2f}")
+print(f"area={area:.2f}")
+'''
